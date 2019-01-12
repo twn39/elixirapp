c8cf838820622291d1bce64e215fd3ab978db2e0
twn39 <twn39@163.com>
2019-01-13 00:34:41 +0800

    add files

---
+++ lib/app.ex
@@ -0,0 +1,24 @@
+defmodule App do
+  @moduledoc """
+  Documentation for App.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> App.hello()
+      :world
+
+  """
+  def start do
+    IO.puts("start...")
+  end
+end
+
+defmodule App.CLI do
+  def main(_args) do
+    App.start()
+  end
+end
